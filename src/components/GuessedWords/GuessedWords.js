92a54cf9021a8228042d1b28bb0ec09141745fe9
Toni Domazet <tdomazet@shutterstock.com>
2019-07-24 16:49:20 +0100

    add code for no words guessed - instructions added

--- src/components/GuessedWords/GuessedWords.js
+++ src/components/GuessedWords/GuessedWords.js
@@ -8,9 +8,13 @@ import PropTypes from 'prop-types';
  * @returns {JSX.Element}
  */
 const GuessedWords = props => {
-  return (
-  <div></div>
-  );
+  let contents;
+  if (!props.guessedWords.length) {
+    contents = (
+      <span data-test="guess-instructions">Try to guess the secret word!</span>
+    );
+  }
+  return <div data-test="component-guessed-words">{contents}</div>;
 };
 
 export default GuessedWords;
