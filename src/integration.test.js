f48d17d2c66835e8c3647084b534049f98377cd2
Toni Domazet <tdomazet@shutterstock.com>
2019-08-02 11:19:37 +0100

    set up guessedWord tests

---
+++ src/integration.test.js
@@ -0,0 +1,21 @@
+import { storeFactory } from '../test/testUtils';
+import { guessedWord } from './actions';
+
+describe('guessedWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    it('updates state correctly for successful guess', () => {
+
+    });
+    it('updates state correctly for unsuccessful guess', () => {
+
+    });
+  });
+  describe('guessed words', () => {
+    it('updates state correctly for successful guess', () => {
+
+    });
+    it('updates state correctly for unsuccessful guess', () => {
+
+    });
+  });
+});
---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function guessedWordsReducer
+ * @param {array} state
+ * @param {object} action
+ * @returns {array} - new guessedWords state.
+ */
+export default (state, action) => {
+  return null;
+};
