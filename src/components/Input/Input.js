4ad4d1ad670bb491d3ec7b3a2ed00cc6c863ef51
Toni Domazet <tdomazet@shutterstock.com>
2019-08-09 10:31:09 +0100

    testing `Input` redux props

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { guessWord } from '../../actions';
+
 /**
  * class component for word.
  * @function Input
@@ -37,4 +39,7 @@ const mapStateToProps = ({ success }) => {
   return { success };
 };
 
-export default connect(mapStateToProps)(Input);
+export default connect(
+  mapStateToProps,
+  { guessWord }
+)(Input);
